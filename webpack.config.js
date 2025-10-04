cb351faa12a5b6f50aac9ecb89c008ad779978f3
Yar <mail@seredkin-pro.ru>
2025-10-04 11:04:02 +0500

    Обновлены файлы в папке src

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.css$/i,
+        test: /\.css$/,
         use: [
           MiniCssExtractPlugin.loader, 
           'style-loader', 
